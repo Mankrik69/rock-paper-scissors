5e4a1c1e3d0318b9b38c52c8b187a84a0b69f782
Mankrik69 <mankrik69@gmail.com>
2024-01-27 00:13:03 +0700

    Remove alerts() and add 3 buttons instead prompt()

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let choice = ["rock", "paper", "scissors"];
+    const choice = ["rock", "paper", "scissors"];
 
     return choice[randomInteger(0, 2)];
 }
@@ -13,65 +13,66 @@ function playRound(playerSelection, computerSelection) {
 
         case "rock":
             if (computerSelection === "scissors") {
-                alert("You Win! Rock beats Scissors");
-                playerScore++;
+                // alert("You Win! Rock beats Scissors");
+                // playerScore++;
                 break;
             } else if (computerSelection === "paper") {
-                alert("You Lose! Paper beats Rock");
-                computerScore++;
+                // alert("You Lose! Paper beats Rock");
+                // computerScore++;
                 break;
             } else {
-                alert("A Tie! Replaying round");
-                playRound(prompt("Your turn"), getComputerChoice());
+                // alert("A Tie! Replaying round");
+                // playRound(prompt("Your turn"), getComputerChoice());
                 break;
             }
 
         case "paper":
             if (computerSelection === "rock") {
-                alert("You Win! Paper beats Rock");
-                playerScore++;
+                // alert("You Win! Paper beats Rock");
+                // playerScore++;
                 break;
             } else if (computerSelection === "scissors") {
-                alert("You Lose! Scissors beat Paper");
-                computerScore++;
+                // alert("You Lose! Scissors beat Paper");
+                // computerScore++;
                 break;
             } else {
-                alert("A Tie! Replaying round");
-                playRound(prompt("Your turn"), getComputerChoice());
+                // alert("A Tie! Replaying round");
+                // playRound(prompt("Your turn"), getComputerChoice());
                 break;
             }
 
         case "scissors":
             if (computerSelection === "paper") {
-                alert("You Win! Scissors beat Paper");
-                playerScore++;
+                // alert("You Win! Scissors beat Paper");
+                // playerScore++;
                 break;
             } else if (computerSelection === "rock") {
-                alert("You Lose! Rock beats Scissors");
-                computerScore++;
+                // alert("You Lose! Rock beats Scissors");
+                // computerScore++;
                 break;
             } else {
-                alert("A Tie! Replaying round");
-                playRound(prompt("Your turn"), getComputerChoice());
+                // alert("A Tie! Replaying round");
+                // playRound(prompt("Your turn"), getComputerChoice());
                 break;
             }
     }
 }
 
-function game() {
-    while (playerScore < 3 && computerScore < 3) {
-        playRound(prompt("Your turn"), getComputerChoice());
-        alert(`Current score: ${playerScore} - ${computerScore}`);
-    }
 
-    if (playerScore === 3) {
-        alert(`You have Won! Final score: ${playerScore} - ${computerScore}`);
-    } else if (computerScore === 3) {
-        alert(`You have Lost! Final score: ${playerScore} - ${computerScore}`);
-    }
-}
+// function game() {
+//     while (playerScore < 3 && computerScore < 3) {
+//         playRound(prompt("Your turn"), getComputerChoice());
+//         alert(`Current score: ${playerScore} - ${computerScore}`);
+//     }
 
-let playerScore = 0;
-let computerScore = 0;
+//     if (playerScore === 3) {
+//         alert(`You have Won! Final score: ${playerScore} - ${computerScore}`);
+//     } else if (computerScore === 3) {
+//         alert(`You have Lost! Final score: ${playerScore} - ${computerScore}`);
+//     }
+// }
 
-game();
+// let playerScore = 0;
+// let computerScore = 0;
+
+// game();
